32725be4308321b6dcf2d8c29b8b9658b328a561
EricWeber33 <edweber@ualberta.ca>
2023-05-09 09:02:44 -0600

    add cors to backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const router = require("./api/employees/employee.router")
 
 app.use(express.json());
+app.use(cors());
 app.use("/api", router);
 app.listen(process.env.SERVER_PORT, () => {
     console.log("Server running")
